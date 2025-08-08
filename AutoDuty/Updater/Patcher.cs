026a4fd7f6403ae5404da9658f421ec2d9e3f3f8
lsl1225 <24491036+lsl1225@users.noreply.github.com>
2025-08-09 01:28:39 +0800

    fix: revert changes and redir main repo

--- AutoDuty/Updater/Patcher.cs
+++ AutoDuty/Updater/Patcher.cs
@@ -33,30 +33,30 @@ namespace AutoDuty.Updater
             PatcherTask = null;
         }
 
-        public static bool PatchTask(bool skipMD5)
+        public static async Task<bool> PatchTask(bool skipMD5)
         {
             Svc.Log.Info("Patching Started");
             try
             {
-                // FIXME: temporary disable md5 check
-                //var localFileInfos = Plugin.PathsDirectory.EnumerateFiles("*.json", SearchOption.AllDirectories);
-                //var localFilesDictionary = localFileInfos.ToDictionary(
-                //    fileInfo => fileInfo.Name,
-                //    fileInfo => BitConverter.ToString(FileHelper.CalculateMD5(fileInfo.FullName)).Replace("-", "")
-                //);
-                //var list = await GitHubHelper.GetPathFileListAsync();
-                //if (list == null) return false;
+                var localFileInfos = Plugin.PathsDirectory.EnumerateFiles("*.json", SearchOption.AllDirectories);
+                var localFilesDictionary = localFileInfos.ToDictionary(
+                    fileInfo => fileInfo.Name,
+                    fileInfo => BitConverter.ToString(FileHelper.CalculateMD5(fileInfo.FullName)).Replace("-", "")
+                );
+                var list = await GitHubHelper.GetPathFileListAsync();
+                if (list == null)
+                    return false;
 
-                //HashSet<string> doNotUpdatePathFiles = ConfigurationMain.Instance.GetCurrentConfig.DoNotUpdatePathFiles;
+                HashSet<string> doNotUpdatePathFiles = ConfigurationMain.Instance.GetCurrentConfig.DoNotUpdatePathFiles;
 
-                //var downloadList = list.Where(kvp => !doNotUpdatePathFiles.Contains(kvp.Key) && (!localFilesDictionary.ContainsKey(kvp.Key) || !localFilesDictionary[kvp.Key].Equals(kvp.Value, StringComparison.OrdinalIgnoreCase)));
+                var downloadList = list.Where(kvp => !doNotUpdatePathFiles.Contains(kvp.Key) && (!localFilesDictionary.ContainsKey(kvp.Key) || !localFilesDictionary[kvp.Key].Equals(kvp.Value, StringComparison.OrdinalIgnoreCase)));
 
-                //foreach (var file in downloadList)
-                //{
-                //    var result = await GitHubHelper.DownloadFileAsync($"https://raw.githubusercontent.com/ffxivcode/AutoDuty/refs/heads/master/AutoDuty/Paths/{file.Key}",$"{Plugin.PathsDirectory.FullName}/{file.Key}");
-                //    var logger = result ? $"Succesfully downloaded: {file.Key}" : $"Failed to download: {file.Key}";
-                //    Svc.Log.Info(logger);
-                //}
+                foreach (var file in downloadList)
+                {
+                    var result = await GitHubHelper.DownloadFileAsync($"https://raw.githubusercontent.com/lsl1225/AutoDutyCN/refs/heads/master/AutoDuty/Paths/{file.Key}", $"{Plugin.PathsDirectory.FullName}/{file.Key}");
+                    var logger = result ? $"Succesfully downloaded: {file.Key}" : $"Failed to download: {file.Key}";
+                    Svc.Log.Info(logger);
+                }
                 return true;
             }
             catch (Exception ex)

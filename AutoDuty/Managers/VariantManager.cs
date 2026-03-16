fd5f665094687932523d68485b5d98928e7e0628
Simon Latusek <Simon.Latusek@gmail.com>
2026-03-16 23:40:30 +0100

    adjusting default path selection

--- AutoDuty/Managers/VariantManager.cs
+++ AutoDuty/Managers/VariantManager.cs
@@ -17,8 +17,8 @@ namespace AutoDuty.Managers
         {
             if (content.VVDIndex < 0)
                 return;
-            _taskManager.Enqueue(() => Svc.Log.Info($"Queueing Duty: {content.Name}"), "RegisterVariantDuty");
-            _taskManager.Enqueue(() => Svc.Log.Info($"Index#: {content.VVDIndex}"), "RegisterVariantDuty");
+            _taskManager.Enqueue(() => Svc.Log.Info($"Queueing Duty: {content.Name}"),   "RegisterVariantDuty");
+            _taskManager.Enqueue(() => Svc.Log.Info($"Index#: {content.VVDIndex}"),      "RegisterVariantDuty");
             _taskManager.Enqueue(() => Plugin.action = $"Queueing Duty: {content.Name}", "RegisterVariantDuty");
             AtkUnitBase* addon = null;
             AtkUnitBase* yesno = null;
@@ -28,22 +28,26 @@ namespace AutoDuty.Managers
                 _taskManager.Enqueue(() => PlayerHelper.IsValid, "RegisterVariantDuty", new TaskManagerConfiguration(int.MaxValue));
                 _taskManager.EnqueueDelay(2000);
             }
-            _taskManager.Enqueue((Action) (() => GenericHelpers.TryGetAddonByName("VVDFinder", out addon)),                            "RegisterVariantDuty");
-            _taskManager.Enqueue(() => { if (addon == null) OpenVVD(); },                                                            "RegisterVariantDuty");
+            _taskManager.Enqueue(() => Plugin.VariantPath = 0);
+            _taskManager.Enqueue((Action)(() => GenericHelpers.TryGetAddonByName("VVDFinder", out addon)), "RegisterVariantDuty");
+            _taskManager.Enqueue(() =>
+                                 {
+                                     if (addon == null) OpenVVD();
+                                 }, "RegisterVariantDuty");
             _taskManager.Enqueue(() => GenericHelpers.TryGetAddonByName("VVDFinder", out addon) && GenericHelpers.IsAddonReady(addon), "RegisterVariantDuty");
-            _taskManager.Enqueue(() => AddonHelper.FireCallBack(addon, true, 13, content.VVDIndex+1),                                  "RegisterVariantDuty");
+            _taskManager.Enqueue(() => AddonHelper.FireCallBack(addon, true, 13, content.VVDIndex + 1),                                "RegisterVariantDuty");
             _taskManager.EnqueueDelay(500);
             _taskManager.Enqueue(() => AddonHelper.FireCallBack(addon, true, 12, 1), "RegisterVariantDuty");
             _taskManager.EnqueueDelay(500);
             _taskManager.Enqueue(() => AddonHelper.FireCallBack(addon, true, 4, 0), "RegisterVariantDuty");
             _taskManager.EnqueueDelay(500);
-            _taskManager.Enqueue(() => GenericHelpers.TryGetAddonByName("SelectYesno", out yesno) && GenericHelpers.IsAddonReady(yesno), "RegisterVariantDuty");
-            _taskManager.Enqueue(() => AddonHelper.FireCallBack(yesno, true, 0, 1), "RegisterVariantDuty");
+            _taskManager.Enqueue(() => GenericHelpers.TryGetAddonByName("SelectYesno", out yesno) && GenericHelpers.IsAddonReady(yesno),           "RegisterVariantDuty");
+            _taskManager.Enqueue(() => AddonHelper.FireCallBack(yesno, true, 0, 1),                                                                "RegisterVariantDuty");
             _taskManager.Enqueue(() => GenericHelpers.TryGetAddonByName("ContentsFinderConfirm", out addon) && GenericHelpers.IsAddonReady(addon), "RegisterVariantDuty");
-            _taskManager.Enqueue(() => AddonHelper.FireCallBack(addon, true, 8), "RegisterVariantDuty");
+            _taskManager.Enqueue(() => AddonHelper.FireCallBack(addon, true, 8),                                                                   "RegisterVariantDuty");
         }
 
-        private static unsafe void OpenVVD() => 
+        private static unsafe void OpenVVD() =>
             AgentModule.Instance()->GetAgentByInternalId(AgentId.VVDFinder)->Show();
 
         public static unsafe bool SelectPath(int option)
@@ -56,18 +60,18 @@ namespace AutoDuty.Managers
             {
                 Svc.Log.Error($"Failed to select path, option {option} is out of range. Entry count: {voteRoute.EntryCount}");
                 return true;
-    }
+            }
 
             if (option == 0)
             {
                 Plugin.VariantPath = voteRoute.SelectedEntryNPC switch
                 {
                     1 => 1,
-                    2 => 5,
-                    3 => 9,
+                    2 => 10,
+                    3 => 6,
                     _ => 1
                 };
-}
+            }
 
             voteRoute.Entries[option].Select();
             return true;
